8d0965441ea9ccbee8107024e56e25922411a1c1
Naman <80120410+namanmodi260@users.noreply.github.com>
2023-01-14 14:04:47 -0500

    Added Text Messages Functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 import firebase from "firebase/app";
@@ -8,18 +9,76 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
-  //API Config here
+  apiKey: "AIzaSyDwy1O1nQFq9TXao6ql2nUQ2-QxD-K2MUo",
+  authDomain: "chat-app-1f60f.firebaseapp.com",
+  projectId: "chat-app-1f60f",
+  storageBucket: "chat-app-1f60f.appspot.com",
+  messagingSenderId: "332612391340",
+  appId: "1:332612391340:web:87cd9435e8bc20384d2a09",
 });
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </>
+  );
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </main>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+  return <p>{text}</p>;
 }
 
 export default App;
